9c0f0db8bbcf1334dd34f90a369fb32879a05769
fantomroot <143911173+fantomroot@users.noreply.github.com>
2023-12-27 18:50:22 +0100

    Add files via upload

--- task2/task2.py
+++ task2/task2.py
@@ -1,1 +1,10 @@
-
+print('Введите координаты и радиус')
+x = float(input("x = "))
+y = float(input("y = "))
+r = float(input("r = "))
+import math
+hypot = math.sqrt(x**2 + y**2)
+if hypot <= r:
+    print("Точка принадлежит кругу")
+else:
+    print("Точка вне круга") 
